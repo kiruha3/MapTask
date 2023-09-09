3e606ff3381610f811abbf22d3bfff7c5145ee15
kirill <kiruha3@mail.ru>
2023-09-09 12:32:24 +0700

    first commit

---
+++ src/main/java/com/kiruha/maptask/MaptaskApplication.java
@@ -0,0 +1,13 @@
+package com.kiruha.maptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaptaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiruha/maptask/MaptaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiruha.maptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaptaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
