265332719419b57b91d048d12a28ec3ff40646ea
kirill <kiruha3@mail.ru>
2023-09-17 21:45:17 +0700

    Исправил все пометки

--- src/main/java/com/kiruha/maptask/controller/department/DepartmentInterface.java
+++ src/main/java/com/kiruha/maptask/controller/department/DepartmentInterface.java
@@ -1,12 +1,10 @@
 package com.kiruha.maptask.controller.department;
 
 import com.kiruha.maptask.Employee;
-import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-@Service
 public interface DepartmentInterface {
 
     Employee minSalary(Integer departmentId);
